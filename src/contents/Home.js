59ba9be6c14d4c1a59d5071a868920e82ea72e0c
MGcommits <marthagamez211@gmail.com>
2021-06-28 21:26:13 -0500

    Create Home component

---
+++ src/contents/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ReactTypingEffect from "react-typing-effect";
+import profilepic from "../img/naafi_photo.png";
+class Home extends Component {
+  render() {
+    return (
+      <div className="condiv home">
+        <img src={profilepic} className="profilepic"></img>
+        <ReactTypingEffect
+          className="typingeffect"
+          text={["I am Martha Gamez", "I am a web developer"]}
+          speed={100}
+          eraseDelay={700}
+        />
+      </div>
+    );
+  }
+}
+export default Home;
