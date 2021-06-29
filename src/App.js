e5545b6a56dfcc2be7aad0219a49046ee380bc52
MGcommits <marthagamez211@gmail.com>
2021-06-29 10:38:07 -0500

    Create About component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./contents/Home";
+import About from "./contents/About";
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        {/* Route for Home.js contents */}
         <Route exact path="/">
           <Home />
         </Route>
-        
+        {/* Route for About.js contents */}
+        <Route path="/about">
+          <About />
+        </Route>
       </div>
     </Router>
   );
---
+++ src/contents/About.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+class About extends Component {
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">About Me</h1>
+        <h4>Hey there,</h4>
+        <h1>I'm Martha Gamez</h1>
+        <h3>
+          Full Stack Web <u>Developer</u> <u>Electrical Engineer</u>
+        </h3>
+        <br></br>
+        <p>
+          I am an Electrical Engineer and a Full Stack Web Developer. 
+          I started my journey in the world of web development by Pursuing 
+          my Full Stack Web Development Certificate at The University of Texas 
+          in Austin.I like creating Interactive UI components for better UX
+          and share those desgin and codes to the world through Github.
+        </p>
+      </div>
+    );
+  }
+}
+export default About;
