f27d0056736347d4a2111eaa33b03c79f0f82078
MGcommits <marthagamez211@gmail.com>
2021-06-29 11:01:59 -0500

    Create Education component

---
+++ src/contents/Education.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Widecard from '../components/Widecard';
+class Education extends Component {
+render() {
+return (
+<div className="condiv">
+<h1 className="subtopic">My Education</h1>
+<Widecard title="Full Stack Web Developer" where="University of Texas" from="June 2021" to="Present"/>
+<Widecard title="Electrical Engineering" where="ITESM" from="1989" to="1994"/>
+</div>
+)
+}
+}
+export default Education
